5d030bad8d46a7999dd95833e4bd20d64dc7dd77
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-07-14 10:16:37 -0500

    Activa estilo al presionar tecla de acuerdo al estado
    Aclaración:
    Los estados para una tecla son:
     - Presionado: blanco
     - Acertado: verde
     - Errado: rojo

--- js/simon-dice.js
+++ js/simon-dice.js
@@ -4,6 +4,21 @@
 function getElementByKeyCode( keyCode ) {
     return document .querySelector( `[data-key="${keyCode}"]` );
 }
-/* keyCode 65 representa a la */
-const el = getElementByKeyCode( 65 );  // a
-console .log( el );
+
+// Activa estilo al presionar tecla de acuerdo al estado
+function activateEl( keyCode, options = {} ) {
+    const el = getElementByKeyCode( keyCode );
+    el .classList .add( 'active' );         // Agrega una clase al listado de clases del elemento
+
+    if( options .success ) {
+        el .classList .add( 'success' );    // Agrega una clase al listado de clases del elemento
+    }
+    else if( options .fail ) {
+        el .classList .add( 'fail' );       // Agrega una clase al listado de clases del elemento
+    }
+}
+
+/* Prueba */
+activateEl( 65 );                       // a
+activateEl( 83, { success: true } );    // s
+activateEl( 68, { fail: true } );       // d
