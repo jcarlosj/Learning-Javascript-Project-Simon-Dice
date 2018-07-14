47cf9cab630177e5db551a18b606cf3ca6167b58
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-07-14 10:52:39 -0500

    Genera 'keyCodes' de las teclas de la 'A' a la 'Z' aleatoriamente

--- js/simon-dice.js
+++ js/simon-dice.js
@@ -1,4 +1,24 @@
 /* Simón Dice: */
+const levels = 15;
+
+// Genera una secuencia de keyCodes aleatorios de teclas 'a' a la 'z'(65 a 90)
+function generateKeys( levels ) {
+    // Retorna un Array con los valores aleatorios en cada una de las posiciones definidas ( levels )
+    return new Array( levels ) .fill( 0 ) .map( generateRandomKey );
+    /* NOTA:
+        - new Array( levels ): Instancia un Objeto de tipo 'Array' con una cantidad de posiciones definidas.
+        - fill( 0 ): llena el Array con CEROS, eso se hace por que no se puede mapear un array vacío.
+        - map( generateRandomKey ): 'Callback' con el que se mapean cada uno de los campos y se le asigna el valor retornado por el mismo.
+    */
+}
+
+// Genera aleatoriamente un número entre 65 y 90
+function generateRandomKey() {
+    const minKeyCode = 65,      // a
+          maxKeyCode = 90;      // z
+
+    return Math .round( Math .random() * ( maxKeyCode - minKeyCode ) + minKeyCode );
+}
 
 // Obtiene el elemento HTML que tenga el keyCode respectivo
 function getElementByKeyCode( keyCode ) {
@@ -30,3 +50,4 @@ function deactivateEl( el ) {
 activateEl( 65 );                       // a
 activateEl( 83, { success: true } );    // s
 activateEl( 68, { fail: true } );       // d
+console .log( 'Teclas generadas', keys = generateKeys( levels ) );
